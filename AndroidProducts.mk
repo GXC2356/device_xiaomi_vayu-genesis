1ead76dfcea434257b57dc842c56c00262ae99dd
GXC2356 <gxcdev2356@gmail.com>
2024-04-13 05:20:47 +0200

    Intial colt setup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vayu.mk
+    $(LOCAL_DIR)/colt_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vayu-eng \
-    aosp_vayu-userdebug \
-    aosp_vayu-user
+    colt_vayu-eng \
+    colt_vayu-userdebug \
+    colt_vayu-user
--- colt_vayu.mk
+++ colt_vayu.mk
@@ -11,20 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Aosp configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common Colt configurations
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_CALL_RECORDING := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+COLT_BUILD_MAINTAINER := GXC2356
+WITH_GAPPS := true
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := colt_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
